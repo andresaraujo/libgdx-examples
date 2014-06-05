7a62b393d8c0817fca8ffbfe33b365fe656bd4d1
Andres Araujo <a.araujo.azua@gmail.com>
2014-06-05 13:22:03 -0500

    Initial commit

---
+++ google-play-signing/android/src/io/github/andresaraujo/gplaysigning/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.github.andresaraujo.gplaysigning.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.github.andresaraujo.gplaysigning.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
