a924e6371864747cde357e810474a1998133eae3
Andres Araujo <a.araujo.azua@gmail.com>
2014-06-14 21:25:03 -0500

    Clean up

--- google-play-gaming-services/android/src/io/github/andresaraujo/gplaygamingservices/android/AndroidLauncher.java
+++ google-play-gaming-services/android/src/io/github/andresaraujo/gplaygamingservices/android/AndroidLauncher.java
@@ -12,7 +12,6 @@ import io.github.andresaraujo.gplaygamingservices.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
 
-    //private GameHelper gameHelper;
     private GooglePlayGameService gpgs;
 
     public AndroidLauncher(){}
@@ -23,31 +22,24 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         gpgs =  new GooglePlayGameService(this);
 		initialize(new MyGdxGame(gpgs), config);
-
-        /*this.gameHelper =  new GameHelper(this, GameHelper.CLIENT_GAMES);
-        this.gameHelper.enableDebugLog(true);
-        this.gameHelper.setup(this);*/
 	}
 
     @Override
     public void onStart(){
         super.onStart();
         gpgs.getGameHelper().onStart(this);
-        //this.gameHelper.onStart(this);
     }
 
     @Override
     public void onStop(){
         super.onStop();
         gpgs.getGameHelper().onStop();
-        //this.gameHelper.onStop();
     }
 
     @Override
     public void onActivityResult(int request, int response, Intent data) {
         super.onActivityResult(request, response, data);
         gpgs.getGameHelper().onActivityResult(request, response, data);
-        //this.gameHelper.onActivityResult(request, response, data);
 
     }
 }
